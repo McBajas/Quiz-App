5c29593eb515564d6166008ab7c7dfeb832a6908
McBajas <pelumijohnson009@gmail.com>
2024-10-18 21:31:10 +0100

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,105 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the capital of Ireland?",
+    answer: "Dublin",
+    options: [
+      "Kenya",
+      "Angola",
+      "Dublin",
+      "Spain"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is the capital of Canada?",
+    answer: "Ottawa",
+    options: [
+      "Onatario",
+      "Ottawa",
+      "Dublin",
+      "Toronto"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What colors is the flag of the United Nations?",
+    answer: "Blue and White",
+    options: [
+      "Blue and White",
+      "Red and White",
+      "Green and Blue",
+      "All White"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Where did the first modern Summer Olympic games take place in 1896?",
+    answer: "Greece",
+    options: [
+      "Spain",
+      "Chile",
+      "Greece",
+      "Australia"
+    ]
+  },
+  {
+    numb: 7,
+    question: "What is the nickname for Arsenal FC?",
+    answer: "The Gunners",
+    options: [
+      "Red Devils",
+      "Emirates",
+      "Gooners",
+      "The Gunners"
+    ]
+  },
+  {
+    numb: 8,
+    question: "What is the the square root of 16?",
+    answer: "4",
+    options: [
+      "4",
+      "8",
+      "7",
+      "3"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
